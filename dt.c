80579ca2e274aefc2c9f0f5c52945244c9b9bbd7
khatrisnehal <khatrisnehal1803@gmail.com>
2024-06-24 12:02:05 +0530

    Add files via upload

---
+++ dt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+//data types
+//variable
+void main()
+{
+	int a,b;
+	a=10;
+	b=30;
+	printf("%d + %d = %d\n",a,b,a+b);
+	printf("%d - %d = %d\n",a,b,a-b);
+	printf("%d * %d = %d\n",a,b,a*b);
+	printf("%d / %d = %d\n",a,b,a/b);
+	
+}
---
+++ qu.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+ void main()
+ {
+ 	int a;
+ 	int b;
+ 	a=5;
+ 	b=5;
+ 	
+ 	printf("- - - - - - - - -\n");
+ 	printf("              |\n");
+ 	printf("%d * %d = %d|\n",a,b,a*b);
+ 	printf("|              \n");
+ 	printf("- - - - - - - - -\n");
+ }
