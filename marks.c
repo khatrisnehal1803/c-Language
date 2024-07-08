62d5703889221e23ea20247fbf43edd407a25da3
khatrisnehal <khatrisnehal1803@gmail.com>
2024-07-08 12:36:14 +0530

    Add files via upload

---
+++ marks.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int sub1,sub2,sub3,average;
+	
+	
+	printf("enter a mark of sub1 =");
+	scanf("%d",&sub1);
+	
+	printf("enter a mark of sub2 =");
+	scanf("%d",&sub2);
+	
+	printf("enter a mark of sub3 =");
+	scanf("%d",&sub3);
+	
+	average = sub1+sub2+sub3;
+	
+	printf(" average mark:%d\n",average);
+	
+	
+}
---
+++ minimum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int x,y;
+	
+	printf("enter the valu of x =");
+	scanf("%d",&x);
+	
+	printf("enter the valu of y =");
+	scanf("%d",&y);
+	
+	if (x,y)
+	{
+		printf("x is minimum");
+	}
+	
+	else
+	{
+		printf("y is minimum");
+	}
+	
+}
